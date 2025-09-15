0b1de4c591777f8edf40cb8b3b28a2ee803cd17b
Denis Macedo <denisrmacedo@gmail.com>
2025-09-15 15:50:52 -0300

    Implementar os beans de configuração dos casos de uso

---
+++ src/main/java/com/poc/hexagonal/config/DeleteCustomerByIdConfig.java
@@ -0,0 +1,22 @@
+package com.poc.hexagonal.config;
+
+import com.poc.hexagonal.adapters.out.DeleteCustomerByIdAdapter;
+import com.poc.hexagonal.application.core.usecase.DeleteCustomerByIdUseCase;
+import com.poc.hexagonal.application.core.usecase.FindCustomerByIdUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DeleteCustomerByIdConfig {
+
+    @Bean
+    public DeleteCustomerByIdUseCase deleteCustomerByIdUseCase(
+        FindCustomerByIdUseCase findCustomerByIdUseCase,
+        DeleteCustomerByIdAdapter deleteCustomerByIdAdapter
+    ) {
+        return new DeleteCustomerByIdUseCase(
+                findCustomerByIdUseCase,
+                deleteCustomerByIdAdapter
+        );
+    }
+}
